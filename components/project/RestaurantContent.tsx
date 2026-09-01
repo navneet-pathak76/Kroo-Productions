cc5850270fd83816e8a082e8b94454c14f4122cb
Navneet Kumar Pathak <99376002+navneet-pathak76@users.noreply.github.com>
2026-09-01 20:28:25 +0530

    Force restaurant gallery heading to two clean lines

--- components/project/RestaurantContent.tsx
+++ components/project/RestaurantContent.tsx
@@ -36,7 +36,7 @@ export default async function RestaurantContentPage() {
     videos,
     featuredButtonLabel: "View projects",
     gallery: {
-      title: <>A CURATED COLLECTION OF OUR<br />CINEMATIC RESTAURANT PRODUCTIONS</>,
+      title: <><span className="block">A CURATED COLLECTION OF OUR</span><span className="block">CINEMATIC RESTAURANT PRODUCTIONS</span></>,
       copy: "Brand films, chef stories and food-forward social edits built to make every dish feel irresistible.",
     },
     about: {
